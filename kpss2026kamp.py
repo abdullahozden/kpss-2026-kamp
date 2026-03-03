0b28405c134d5fbd4f95582e4b70326d4ac9f7ba
abdullahozden <126578994+abdullahozden@users.noreply.github.com>
2026-03-03 21:12:10 +0300

    Update kpss2026kamp.py

--- kpss2026kamp.py
+++ kpss2026kamp.py
@@ -151,6 +151,9 @@ elif menu == "📅 Günlük Planım":
     col_t, col_tog = st.columns([4, 1])
     with col_t: st.subheader("📅 Görev Takibi")
     with col_tog: show_history = st.toggle("📜 Arşiv")
+
+        display_df = user_df[user_df['tamamlandi'] == show_history]
+        display_df = display_df[display_df['konu'] != "Hesap Aktif"]
     
     if show_history:
         for idx, row in display_df.sort_values(by="tarih", ascending=False).iterrows():
@@ -214,4 +217,5 @@ elif menu == "🏆 Başarılarım":
             with st.expander("Detaylar"):
                 for _, b in b_df.iterrows():
                     st.markdown(f"<div class='success-card'><b>{b['konu']}</b>: {int(b['soru_cozulen'])} soru çözüldü.</div>", unsafe_allow_html=True)
+
 
