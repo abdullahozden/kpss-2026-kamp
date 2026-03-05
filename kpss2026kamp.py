59eec92c309646ce256a66a88874c408ffbad9ec
abdullahozden <126578994+abdullahozden@users.noreply.github.com>
2026-03-05 14:12:56 +0300

    Update kpss2026kamp.py

--- kpss2026kamp.py
+++ kpss2026kamp.py
@@ -181,13 +181,11 @@ with st.sidebar.expander("⚙️ Hesap Ayarları"):
         if col_del2.button("İPTAL", use_container_width=True):
             st.session_state.confirm_delete = False
             st.rerun()
-    st.markdown("<hr style='margin:4px 0px;'>", unsafe_allow_html=True)
 if st.sidebar.button("🚪 Çıkış Yap", use_container_width=True):
     st.session_state.user = None
     st.rerun()
-
-st.markdown("<hr style='margin:4px 0px;'>", unsafe_allow_html=True)
 menu = st.sidebar.radio("Gezinti", ["📅 Günlük Planım", "📝 Plan Oluştur", "🏆 Başarılarım", "📊 Deneme Takibi"])
+st.markdown("<hr style='margin:4px 0px;'>", unsafe_allow_html=True)
 st.markdown('<div class="custom-header"><h1>🚀 <span style="color: #58a6ff;">2026 KPSS</span> ÇALIŞMA PLANI</h1></div>', unsafe_allow_html=True)
 
 # --- 6. PLAN OLUŞTUR ---
@@ -467,6 +465,7 @@ elif menu == "📊 Deneme Takibi":
                         st.toast("🗑️  Deneme silindi.")
                         time.sleep(1)
                         st.rerun()
+
 
 
 
