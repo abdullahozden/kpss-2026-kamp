d525ae656623178d777cd48cbe733bfabb813f0f
abdullahozden <126578994+abdullahozden@users.noreply.github.com>
2026-03-05 12:42:11 +0300

    Update kpss2026kamp.py

--- kpss2026kamp.py
+++ kpss2026kamp.py
@@ -420,16 +420,17 @@ elif menu == "📊 Deneme Takibi":
                     st.markdown(f"**{d_row['konu']}**")
                     st.caption(f"📅 {d_row['tarih']}")
                     st.write(f"GK: {d_row['deneme_gk_d']}D {d_row['deneme_gk_y']}Y | GY: {d_row['deneme_gy_d']}D {d_row['deneme_gy_y']}Y")
-                 with col_puan:
+                with col_puan:
                     st.markdown(f"<h3 style='margin:0; color:{color};'>{puan:.2f}</h3>", unsafe_allow_html=True)
                     st.caption(f"Hedefe Uzaklık: {fark:.2f}")
-                 with col_islem:
-                    if st.button("🗑️", key=f"del_deneme_{d_row['id']}", use_container_width=True):
-                        # Seçili ID hariç diğerlerini sakla ve güncelle
-                        save_to_gsheets(all_db[all_db['id'] != d_row['id']])
-                        st.toast("🗑️  Deneme silindi.")
-                        time.sleep(1)
-                        st.rerun()
+                    with col_islem:
+                        if st.button("🗑️", key=f"del_deneme_{d_row['id']}", use_container_width=True):
+                            # Seçili ID hariç diğerlerini sakla ve güncelle
+                            save_to_gsheets(all_db[all_db['id'] != d_row['id']])
+                            st.toast("🗑️  Deneme silindi.")
+                            time.sleep(1)
+                            st.rerun()
+
 
 
 
