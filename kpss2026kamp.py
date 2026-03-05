786aa5e65d9d08a6d41cad8f738361c48d4ab223
abdullahozden <126578994+abdullahozden@users.noreply.github.com>
2026-03-05 04:47:35 +0300

    Update kpss2026kamp.py

--- kpss2026kamp.py
+++ kpss2026kamp.py
@@ -218,7 +218,10 @@ elif menu == "📅 Günlük Planım":
                         save_to_gsheets(all_db); st.rerun()
                 with c_arc_del:
                     if st.button("🗑️", key=f"del_arc_{row['id']}", help="Sil", use_container_width=True):
-                        save_to_gsheets(all_db[all_db['id'] != row['id']]); st.rerun()
+                        save_to_gsheets(all_db[all_db['id'] != row['id']]);
+                        st.toast(f"{row['konu']} başarıyla sildiniz.", icon="🗑️") # Pop-up bildirim
+                        time.sleep(0.5)
+                        st.rerun()
             st.divider()
 
     active_df = user_df[(user_df['tamamlandi'] == False) & (user_df['konu'] != "Hesap Aktif")]
@@ -300,6 +303,7 @@ elif menu == "🏆 Başarılarım":
                 for _, b in b_df.iterrows():
                     v_say = len(json.loads(b['videolar'])) if isinstance(b['videolar'], str) else 0
                     st.markdown(f'<div class="success-card"><b>{b["konu"]}</b><br><small>📝 {int(b["soru_cozulen"])} Soru | 📺 {v_say} Video | 📅 {b["tarih"]}</small></div>', unsafe_allow_html=True)
+
 
 
 
