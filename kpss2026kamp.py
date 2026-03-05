6a5d54760ce9b45096db0ea861f22d20547a344b
abdullahozden <126578994+abdullahozden@users.noreply.github.com>
2026-03-05 04:45:39 +0300

    Update kpss2026kamp.py

--- kpss2026kamp.py
+++ kpss2026kamp.py
@@ -192,8 +192,8 @@ if menu == "📝 Plan Oluştur":
                 st.success("Plan eklendi! Liste güncelleniyor...")
                 time.sleep(1)
                 st.rerun()
-             else:
-                 st.error("Lütfen bir konu ismi girin!")
+            else:
+                st.error("Lütfen bir konu ismi girin!")
 
 # --- 7. GÜNLÜK PLANIM ---
 elif menu == "📅 Günlük Planım":
@@ -300,6 +300,7 @@ elif menu == "🏆 Başarılarım":
                 for _, b in b_df.iterrows():
                     v_say = len(json.loads(b['videolar'])) if isinstance(b['videolar'], str) else 0
                     st.markdown(f'<div class="success-card"><b>{b["konu"]}</b><br><small>📝 {int(b["soru_cozulen"])} Soru | 📺 {v_say} Video | 📅 {b["tarih"]}</small></div>', unsafe_allow_html=True)
+
 
 
 
