8383c59841e32c8a2d6abd2f042f0c1bbc597d3b
abdullahozden <126578994+abdullahozden@users.noreply.github.com>
2026-03-10 17:49:28 +0300

    Update kpss2026kamp.py

--- kpss2026kamp.py
+++ kpss2026kamp.py
@@ -406,6 +406,7 @@ elif menu == "📅 Günlük Planım":
                             st.toast(f"{row['konu']} konusunu sildiniz!", icon="🗑️") # Pop-up bildirim
                             time.sleep(1)
                             st.rerun()
+            st.markdown("<hr style='margin:1px 0px;'>", unsafe_allow_html=True)
 
 # --- 8. BAŞARILARIM ---
 elif menu == "🏆 Başarılarım":
@@ -542,4 +543,5 @@ elif menu == "📊 Deneme Takibi":
                         st.toast("🗑️  Deneme silindi.")
                         time.sleep(1)
                         st.rerun()
+
 
