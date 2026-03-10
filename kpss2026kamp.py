7fea4b379747114dee9e2fda2653dfe68c0df0dc
abdullahozden <126578994+abdullahozden@users.noreply.github.com>
2026-03-10 23:13:51 +0300

    Update kpss2026kamp.py

--- kpss2026kamp.py
+++ kpss2026kamp.py
@@ -201,6 +201,7 @@ with st.sidebar.expander("⚙️ Hesap Ayarları"):
         all_db.loc[all_db['username'] == username, 'display_name'] = yeni_display
         save_to_gsheets(all_db)
         st.success("İsim güncellendi!")
+        time.sleep(0.5)
         st.rerun()
         
     # 2. Hedef Puan Belirleme
@@ -210,6 +211,7 @@ with st.sidebar.expander("⚙️ Hesap Ayarları"):
         all_db.loc[all_db['username'] == username, 'puan_hedef'] = yeni_hedef
         save_to_gsheets(all_db)
         st.success("Hedef puan güncellendi! 🎯")
+        time.sleep(0.5)
     if st.button("❌ Hesabımı Sil", type="secondary", use_container_width=True):
         st.session_state.confirm_delete = True
     if st.session_state.get('confirm_delete', False):
@@ -559,3 +561,4 @@ elif menu == "📊 Deneme Takibi":
                         st.toast("🗑️  Deneme silindi.")
                         time.sleep(1)
                         st.rerun()
+
